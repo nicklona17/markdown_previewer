b3007f7e0880317f1e1f3a3811c41bfcb865e541
nicklona17 <nicklona17@gmail.com>
2022-04-26 14:11:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+import { marked } from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdown: `# Header
+
+## Sub Header
+
+[YouTube](https://www.youtube.com)
+
+\`<title>Markdown Previewer</title>\`
+
+    <html>
+      <head>
+      </head>
+    </html>
+
+- This
+- is
+- a
+- List
+
+> I made a markdown previewer.
+
+![freeCodeCamp Logo](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_DXmkUBKMV2TSteYbUzExv4IriEc-E1Xq9OaPlXo2oApzFkQJyJx3Q1_tF6tZXVNN9ms&usqp=CAU)
+
+**bold text**
+`
+    }
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  }
+
+  render() {
+    const editorStyle = {
+      width: "400px",
+      height: "100vh",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: "10px"
+    }
+
+    const previewStyle = {
+      width: "400px",
+      height: "100vh",
+      backgroundColor: "#DCDCDC",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: "10px"
+    }
+
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="rounded-pill bg-dark text-align-center">
+                Markdown Previewer
+                </Badge>
+              </h1>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="rounded-pill bg-dark text-align-center">
+                    Editor
+                  </Badge>
+                </h4>
+              </div>
+              <div className="mark-input" style={editorStyle}>
+              <textarea id="editor" className="input" style={editorStyle} value={this.state.markdown} onChange={(e) => {this.updateMarkdown(e.target.value)}}></textarea>
+              </div>
+            </div>
+            
+            <div className="col">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="rounded-pill bg-dark text-align-center">
+                    Preview
+                  </Badge>
+                </h4>
+              </div>
+              <div id="preview" style={previewStyle} dangerouslySetInnerHTML={{
+                  __html: marked(this.state.markdown)
+                }}>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
